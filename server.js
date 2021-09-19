c97637b511f6e283011b43b3bf927c35746480eb
Adriana <84536824+itsmeadriana@users.noreply.github.com>
2021-09-19 15:06:07 -0500

    trying out

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+// makes public front-end files available
+app.use(express.static('public'));
+
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -110,6 +113,10 @@ app.post('/api/animals', (req, res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(req.body);
     }
+});
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
 });
 
 app.listen(PORT, () => {
